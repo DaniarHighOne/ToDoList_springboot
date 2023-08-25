4cfe9d6df61a50330fc0c8b62c1342e8b1025ae9
Daniar <daniyaruteg123@gmail.com>
2023-08-25 17:40:48 +0500

    added validation for dto and add mappers

--- src/main/java/com/example/taskList/web/dto/auth/JwtRequest.java
+++ src/main/java/com/example/taskList/web/dto/auth/JwtRequest.java
@@ -1,9 +1,14 @@
 package com.example.taskList.web.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class JwtRequest {
+
+    @NotNull(message= "Username couldn't be null")
     private String username;
+
+    @NotNull(message= "Password couldn't be null")
     private String password;
 }
--- src/main/java/com/example/taskList/web/dto/user/UserDto.java
+++ src/main/java/com/example/taskList/web/dto/user/UserDto.java
@@ -1,13 +1,32 @@
 package com.example.taskList.web.dto.user;
 
+import com.example.taskList.web.dto.validation.OnCreate;
+import com.example.taskList.web.dto.validation.OnUpdate;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 
 @Data
 public class UserDto {
+
+    @NotNull(message="Id couldn't be null", groups = OnUpdate.class)//for validation on not null and creating id
     private Long id;
+
+    @NotNull(message="Name couldn't be null", groups= {OnCreate.class,OnUpdate.class})
+    @Length(max=255, message="Name length shuold be less 255 symbols",groups= {OnCreate.class,OnUpdate.class})
     private String name;
+
+    @NotNull(message="Username couldn't be null", groups= {OnCreate.class,OnUpdate.class})
+    @Length(max=255, message="Username length shuold be less 255 symbols",groups= {OnCreate.class,OnUpdate.class})
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)//we can only write it but not send it
+    @NotNull(message="Password must be not null", groups= {OnCreate.class,OnUpdate.class})
     private String password;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)//we can only write it but not send it
+    @NotNull(message="Password must be not null", groups= {OnCreate.class})//here user can update password
     private String passwordConfirmation;
 }
